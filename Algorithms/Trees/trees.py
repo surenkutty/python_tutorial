fcabac84a6dd7e4ed15e5312863e0acf143eb579
surenkutty <105663626+surenkutty@users.noreply.github.com>
2024-12-11 10:00:45 -0800

    disply tree

--- Algorithms/Trees/trees.py
+++ Algorithms/Trees/trees.py
@@ -30,10 +30,31 @@ class Tree:
                 return nodeFound
         
         return None
+    def display(self,depth=0,node=None):
+        if not node:
+            node=self.root
+        print(" "*depth,node.data)
+        for child in node.children:
+            self.display(depth+1,child)
+            
+    def remove(self,data):
+        if not self.remove:
+            print("tree is empty")
+        if self.root.data==data:
+            self.root=None
+            return
+        
+        
                 
 tree=Tree()
-tree.Add(5)        
-tree.Add(5,6)        
-tree.Add(5,3)        
-print(tree)
+tree.Add(1)        
+tree.Add(2,1)        
+tree.Add(3,1)        
+tree.Add('a',1)        
+tree.Add(4,2)        
+tree.Add(5,2)        
+tree.Add(6,3)        
+tree.Add(7,3)        
+tree.display(node=tree.root)
+
         
