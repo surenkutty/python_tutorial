d9d1f13dad32623097d5676b4eb0b2dd9f4324cd
surenkutty <105663626+surenkutty@users.noreply.github.com>
2024-12-29 08:14:48 -0800

    FlippigBits  Day-3

---
+++ HackerRank/FlippingBits.py
@@ -0,0 +1,13 @@
+
+#2**32-1 is bit of unsignedintegers or 0xFFFFFFFF
+
+def flippingBits(n):
+    return 0xFFFFFFFF-n
+    # Write your code here
+
+if __name__ == '__main__':
+    ans=flippingBits(5)
+    print(ans)
+    
+
+    
