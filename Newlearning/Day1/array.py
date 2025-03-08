d23f7e71ad2c24dc04d438819037b7a0551afa20
surenkutty <105663626+surenkutty@users.noreply.github.com>
2025-03-08 14:14:20 +0530

    gitignore

---
+++ Newlearning/Day1/array.py
@@ -0,0 +1,12 @@
+#Array requireds imports
+#its stored only one datatype
+#memory efficent
+#faster than list
+
+import array
+
+import numpy as np
+arr=array.array('i',[1,2,3,4,5])
+
+nparr=np.array([1,2,3,4,5])
+print(nparr)
---
+++ Newlearning/Day1/list.py
@@ -0,0 +1,5 @@
+#stored mixed datatyps
+#memory inefficent
+
+lst=['a',1,2.3,True]
+print(lst)
